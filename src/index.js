28374861fd033eaa3c7a39a507705c313173b74d
William Ramos de Caldas <ra53986960848@cursos.insf>
2023-08-29 14:53:02 -0300

    subindo projeto

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home'
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />}/> 
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
