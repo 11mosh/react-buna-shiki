0874ec7fa2eed189c1804b0e4cab78f414ea9639
Pietro <pietroaug806@gmail.com>
2023-09-16 10:03:42 -0300

    tela inicial adm feita

--- src/Routes.js
+++ src/Routes.js
@@ -9,6 +9,12 @@ import UsuarioLogin from './pages/Usuario/UsuarioLogin';
 import UsuarioCadastro from './pages/Usuario/UsuarioCadastro'
 import Carrinho from './pages/Usuario/Carrinho';
 import AdmLogin from './pages/Admin/AdmLogin';
+import AdmInicio from './pages/Admin/TelaInicial';
+import CadastroProduto from './pages/Admin/CadastroProduto';
+import Estatisticas from './pages/Admin/Estatísticas';
+import Pedidos from './pages/Admin/Pedidos';
+import Produtos from './pages/Admin/Produtos';
+import CadastroCombo from './pages/Admin/CadastroCombo';
 
 
 export default function Rotas() {
@@ -21,10 +27,16 @@ export default function Rotas() {
                 <Route path='/sustentabilidade' element={<Sustentabilidade />}/> 
                 <Route path='/cabeca' element={<CabecalhoAdm />}/>
                 <Route path='/cabecalho' element={<CabecalhoUsuario />}/>
-                <Route path='/usuario/login' element={<UsuarioLogin />}/>
-                <Route path='/usuario/cadastro' element={<UsuarioCadastro />} />
-                <Route path='/usuario/carrinho' element={<Carrinho/>} />
+                <Route path='/login' element={<UsuarioLogin />}/>
+                <Route path='/cadastro' element={<UsuarioCadastro />} />
+                <Route path='/carrinho' element={<Carrinho/>} />
                 <Route path='/adm/login' element={<AdmLogin />} />
+                <Route path='/adm/inicio' element={<AdmInicio />} />
+                <Route path='/adm/cadastro-produto' element={<CadastroProduto />} />
+                <Route path='/adm/produtos' element={<Produtos />} />
+                <Route path='/adm/cadastro-combo' element={<CadastroCombo />} />
+                <Route path='/adm/estatisticas' element={<Estatisticas />} />
+                <Route path='/adm/pedidos' element={<Pedidos />} />
             </Routes>
          </BrowserRouter>
     )
---
+++ src/pages/Admin/TelaInicial/index.jsx
@@ -0,0 +1,48 @@
+import CabecalhoAdm from '../../../components/Admin/AdmCabecalho';
+import './index.scss';
+import {Link} from 'react-router-dom'
+
+export default function Index() {
+    return(
+        <div id='page-adm-inicio'>
+            <CabecalhoAdm />
+            <main>
+                <section id='s1'>
+                    <h1> Tela inicial </h1>
+                </section>
+                <section id='s2'>
+                    <Link to='/adm/cadastro-produtos'>
+                        <div>
+                            <img src='/assets/images/tela-inicial-adm/icon-coffee.svg' alt='icon-coffee' />
+                        </div>
+                        <h3> Adicionar um produto </h3>
+                    </Link>
+                    <Link to='/adm/produtos'>
+                        <div>
+                            <img id='eyes' src='/assets/images/tela-inicial-adm/icon-eyes.svg' alt='icon-eyes' />
+                        </div>
+                        <h3> Visualizar produto </h3>
+                    </Link>
+                    <Link to='/adm/pedidos'>
+                        <div>
+                            <img src='/assets/images/tela-inicial-adm/icon-list.svg' alt='icon-list' />
+                        </div>
+                        <h3> Visualizar pedidos </h3>
+                    </Link>
+                    <Link to='/adm/estatisticas'>
+                        <div>
+                            <img src='/assets/images/tela-inicial-adm/icon-arrow.svg' alt='icon-arrow' />
+                        </div>
+                        <h3> Estatísticas </h3>
+                    </Link>
+                    <Link to='/adm/cadastro-combo'>
+                        <div>
+                            <img src='/assets/images/tela-inicial-adm/icon-box.svg' alt='icon-box' />
+                        </div>
+                        <h3> Adicionar um combo </h3>
+                    </Link>
+                </section>
+            </main>
+        </div>
+    )
+}
