bfefab2bf6e1c8538bf45a89b75e0df3185fd12e
pietro <pietroaug806@gmail.com>
2023-09-28 17:08:10 -0300

    pequenas correções

--- src/components/Usuario/UsuarioCabecalho/index.jsx
+++ src/components/Usuario/UsuarioCabecalho/index.jsx
@@ -26,30 +26,31 @@ export default function CabecalhoUsuario() {
 
     return(
         <div className='comp-usuario-cabecalho'>
-            <section style={{ "display": exibirPesquisa}}>
-                <article>
-                    <img src='/assets/images/icon-carrinho.svg' alt='carrinho'/>
-                    <p> Carrinho </p>
-                </article>
-                <article>
-                    <img src='/assets/images/icon-conta.svg' alt='conta'/>
-                   <p>Conta</p>
-                </article>
-                {/* <article id='invisivel'>
-                    <img src='/assets/images/tresBarras.svg' alt='tres barras' onClick={e => {if(mostrarMenu === false){setMostrarMenu(true)} else{setMostrarMenu(false)}}}/>
-                </article> */}
-            </section>
-            <div id='logo'>
-                <img src='/assets/images/logo.svg'  alt="Erro ao exibir imagem"/>
-            </div>
-            <div id='logoXicara'>
-                <img src='/assets/images/logo-xicara.png'  alt="Erro ao exibir imagem"/>
-            </div>
-            <div className='lupa'>
-                {mostrarInput 
-                    ? (<input type="text" placeholder='Pesquise por produtos aqui...' value={pesquisa} onChange={e => setPesquisa(e.target.value)} onKeyDown={teste} />) 
-                    : ( <></> )}
-                <img src='/assets/images/lupa.svg' alt="Erro ao exibir imagem" onClick={exibirPesquisa}/>
+            <div>
+                <section style={{ "display": exibirPesquisa}}>
+                    <article>
+                        <img src='/assets/images/icon-carrinho.svg' alt='carrinho'/>
+                        <p> Carrinho </p>
+                    </article>
+                    <article>
+                        <img src='/assets/images/icon-conta.svg' alt='conta'/>
+                    <p>Conta</p>
+                    </article>
+                    {/* <article id='invisivel'>
+                        <img src='/assets/images/tresBarras.svg' alt='tres barras' onClick={e => {if(mostrarMenu === false){setMostrarMenu(true)} else{setMostrarMenu(false)}}}/>
+                    </article> */}
+                </section>
+                <div id='logo'>
+                    <img src='/assets/images/logo.svg'  alt="Erro ao exibir imagem"/>
+                </div>
+
+
+                <div className='lupa'>
+                    {mostrarInput 
+                        ? (<input type="text" placeholder='Pesquise por produtos aqui...' value={pesquisa} onChange={e => setPesquisa(e.target.value)} onKeyDown={teste} />) 
+                        : ( <></> )}
+                    <img src='/assets/images/lupa.svg' alt="Erro ao exibir imagem" onClick={exibirPesquisa}/>
+                </div>
             </div>
             <nav className='categorias-nav'>
                 <Link to='/produtos/cafe-em-po'>Café em pó</Link>
