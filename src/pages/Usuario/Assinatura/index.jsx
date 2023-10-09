146137a47a9a975bcdcabd08c2cd737ba21053f2
RamosWill <williamramos4994@gmail.com>
2023-10-09 11:30:57 -0300

    assinatura page

--- src/pages/Usuario/Assinatura/index.jsx
+++ src/pages/Usuario/Assinatura/index.jsx
@@ -14,9 +14,9 @@ export default function Assinatura () {
                 <section className='texto-apresentacao'>
                     <p>Nós selecionamos uma lista dos melhores sabores de café pelos quais você pode se interessar, além de alguns itens que possam ser necessários.  :)</p>
                     <ul>
-                        <li>&bull; Nossos produtos são enviados sempre o mais fresco possível;</li>
-                        <li>&bull; Os produtos têm 5% de desconto ao final do pagamento;</li>
-                        <li>&bull; Muito mais conforto e comodidade.</li>
+                        <li> Nossos produtos são enviados sempre o mais fresco possível;</li>
+                        <li> Os produtos têm 5% de desconto ao final do pagamento;</li>
+                        <li> Muito mais conforto e comodidade.</li>
                     </ul>
                 </section>
 
@@ -28,7 +28,7 @@ export default function Assinatura () {
 
                     <div className="itens-cafe">
                         <div className="item">
-                            <img src="/images/cafeteiraa.png" alt="" />
+                            <img src="/assets/images/cafeteiraa.png" alt="" />
                             <p>Café Orfeu Clássico 1kg</p>
                             <div className='quantidade-item'>
                                 <p>-</p>
@@ -38,7 +38,7 @@ export default function Assinatura () {
                         </div>
 
                         <div className="item">
-                            <img src="/images/cafeteiraa.png" alt="" />
+                            <img src="/assets/images/cafeteiraa.png" alt="" />
                             <p>Café Orfeu Clássico 1kg</p>
                             <div className='quantidade-item'>
                                 <p>-</p>
@@ -48,7 +48,7 @@ export default function Assinatura () {
                         </div>
 
                         <div className="item">
-                            <img src="/images/cafeteiraa.png" alt="" />
+                            <img src="/assets/images/cafeteiraa.png" alt="" />
                             <p>Café Orfeu Clássico 1kg</p>
                             <div className='quantidade-item'>
                                 <p>-</p>
@@ -58,18 +58,44 @@ export default function Assinatura () {
                         </div>
 
                         <div className="item">
-                            <img src="/images/cafeteiraa.png" alt="" />
+                            <img src="/assets/images/cafeteiraa.png" alt="" />
                             <p>Café Orfeu Clássico 1kg</p>
                             <div className='quantidade-item'>
                                 <p>-</p>
                                 <p>1</p>
                                 <p>+</p>
                             </div>
+                        </div>
+
+                        <div className="item">
+                            <img src="/assets/images/cafeteiraa.png" alt="" />
+                            <p>Café Orfeu Clássico 1kg</p>
+                            <div className='quantidade-item'>
+                                <p>-</p>
+                                <p>1</p>
+                                <p>+</p>
+                            </div>
+                        </div>
+
+                        <div className="item">
+                            <img src="/assets/images/cafeteiraa.png" alt="" />
+                            <p>Café Orfeu Clássico 1kg</p>
+                            <div className='quantidade-item'>
+                                <p>-</p>
+                                <p>1</p>
+                                <p>+</p>
+                            </div>
+                        </div>
+
+                        <div className='trocar-produtos'>
+                            <img src="/assets/images/setadropdown.png" alt="" />
                         </div>
                     </div>
                 </section>
 
                 <section className="selecionar-cartao">
+                <h2>2 - Cadastre ou escolha um cartão de crédito:</h2>
+
                     <article className='info-cartao'>
                         <div className='titulo'>
                             <p>Não tem? Cadastre um agora!</p>
@@ -77,7 +103,59 @@ export default function Assinatura () {
                         </div>
                         
                     </article>
-                    <select name="" id=""></select>
+                    <select name="" id="">
+                        <option value="">CEFDS</option>
+                        <option value="">CEFDS</option>
+                        <option value="">CEFDS</option>
+                        <option value="">CEFDS</option>
+                    </select>
+                </section>
+
+                <section className='selecionar-endereco'>
+                    <h2>3 - Cadastre ou escolha um endereço de entrega:</h2>
+                    <article className='info-endereco'>
+                        <div className='titulo'>
+                            <p>Não tem? Cadastre um agora!</p>
+                            &darr;
+                        </div>
+                        <div className="campo1">
+                            <div>
+                                <label htmlFor="">Informe o CEP &nbsp; &nbsp; *</label>
+                                <input type="text" name="" id="" />
+                            </div>
+                            <div>
+                                <label htmlFor="">Informe o número &nbsp; &nbsp; *</label>
+                                <input type="text" />
+                            </div>
+                        </div>
+
+                        <div className="campo2">
+                            <label htmlFor=""></label>
+                            <input type="text" />
+                            <div className='enderecoPreenchido'></div>
+                            <div className='enderecoPreenchido'></div>
+                        </div>
+
+                        <button>Cadastrar</button>
+                    </article>
+                    <select name="Selecionar endereço" id="">
+                        <option value="">CEFDS</option>
+                    </select>
+                </section>
+
+                <button>
+                    Continuar
+                    <img src="/assets/images/icon-seta-longa-esquerda.png" alt="" style={{transform: 'rotate(180deg)', width: '50px'}}/>
+                </button>
+
+                <section className="texto-ajuda">
+                    <h2>Você pode se perguntar:</h2>
+                    <h2>Como funciona a assinatura mensal?</h2>
+                    <p>Nosso método de pagamento nessa modalidade aceita apenas <b>cartão de crédito</b>.</p>
+                    <p>1 - Você escolhe quais produtos deseja levar;</p>
+                    <p>2 - Cadastra o cartão de crédito e efetua o primeiro pagamento</p>
+                    <p>3 - Todos os meses, no mesmo dia em que o primeiro pagamento foi confirmado, uma nova cobrança chegará no cartão cadastrado e nós separaremos os produtos <b>selecionados por você</b>;</p>
+                    <p>4 - A qualquer momento <b>você pode alterar</b> os produtos envolvidos na assinatura, <b>porém</b>, uma vez que o pagamento for confirmado, as alterações só serão atendidas no próximo mês!</p>
                 </section>
             </main>
         </main>
