26f278a583557a5fb66cf7d0d30352c3cf8b5a8c
William Ramos de Caldas <ra53986960848@cursos.insf>
2023-10-26 16:41:39 -0300

    alteracoes

---
+++ src/pages/Usuario/Assinatura/Item/index.jsx
@@ -0,0 +1,32 @@
+import './index.scss';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
+export default function ItemDisponivel (props) {
+
+    const [contador, setContador] = useState(0);
+
+    function diminuir () {
+        if (contador == 0) {
+
+        } else if (contador > 0) {
+            setContador(contador - 1);
+        }
+    }
+
+    return (
+        <main>
+            <div className="item">
+                <div className="imagem">
+                    <img src={props.itemm.imagem} alt="" />
+                </div>
+                <p>{props.itemm.produto}</p>
+                <div className='quantidade-item'>
+                    <p className='adicionar' onClick={diminuir}>-</p>
+                    <p>{contador}</p>
+                    <p className='adicionar' onClick={() => {setContador(contador + 1)}}>+</p>
+                </div>
+            </div>
+        </main>
+    )
+}
