0c85de309f6a9e0c7b420cea15c03c9995b44510
William Ramos de Caldas <ra53986960848@cursos.insf>
2023-11-14 18:07:23 -0300

    pedidos

--- src/pages/Usuario/UsuarioPerfil/MeusPedidos/avaliacao/telaAvaliacao.jsx
+++ src/pages/Usuario/UsuarioPerfil/MeusPedidos/avaliacao/telaAvaliacao.jsx
@@ -1,15 +1,57 @@
 import './custom.scss'
+import { toast } from 'react-toastify';
+import axios from 'axios';
+import { useState } from 'react';
+import { URLRota } from '../../../../../constants.js';
 
-function Avaliacao ({avaliar}) {
+function Avaliacao ({idPedido, fechar}) {
+
+
+     const umaEstrela= async () => {
+        const url = URLRota + '/pedido/' + idPedido +'/avaliacao/' + 1;
+        const resposta = await axios.put(url)
+        toast.info('Produto avaliado com 1 estrela!')
+        fechar()
+    }
+
+    const duasEstrelas = async () => {
+        const url = URLRota + '/pedido/' + idPedido +'/avaliacao/' + 2;
+        const resposta = await axios.put(url)
+        toast.info('Produto avaliado com 2 estrelas!')
+        fechar()
+
+    }
+    const tresEstrelas= async () => {
+        const url = URLRota + '/pedido/' + idPedido +'/avaliacao/' + 3;
+        const resposta = await axios.put(url)
+        toast.info('Produto avaliado com 3 estrelas!')
+        fechar()
+
+    }
+    const quatroEstrelas= async () => {
+        const url = URLRota + '/pedido/' + idPedido +'/avaliacao/' + 4;
+        const resposta = await axios.put(url)
+        toast.info('Produto avaliado com 4 estrelas!')
+        fechar()
+
+    }
+    const cincoEstrelas = async () => {
+        const url = URLRota + '/pedido/' + idPedido + '/avaliacao/' + 5;
+        const resposta = await axios.put(url)
+        toast.info('Produto avaliado com 5 estrelas!')
+        fechar()
+        
+    }
+
     return (
       <div className="corpo-principal-avaliacao">
         <h2>Avalie nosso produto!</h2>
         <div>
-          <img src="/assets/images/Star 22.svg" alt="" onClick={() => avaliar(1)}/>
-          <img src="/assets/images/Star 22.svg" alt="" onClick={() => avaliar(2)}/>
-          <img src="/assets/images/Star 22.svg" alt="" onClick={() => avaliar(3)}/>  
-          <img src="/assets/images/Star 22.svg" alt="" onClick={() => avaliar(4)}/>
-          <img src="/assets/images/Star 22.svg" alt="" onClick={() => avaliar(5)}/>
+          <img src="/assets/images/Star 22.svg" alt="" onClick={umaEstrela}/>
+          <img src="/assets/images/Star 22.svg" alt="" onClick={duasEstrelas}/>
+          <img src="/assets/images/Star 22.svg" alt="" onClick={tresEstrelas}/>  
+          <img src="/assets/images/Star 22.svg" alt="" onClick={quatroEstrelas}/>
+          <img src="/assets/images/Star 22.svg" alt="" onClick={cincoEstrelas}/>
         </div>
       </div>
     );
--- src/pages/Usuario/UsuarioPerfil/MeusPedidos/index.jsx
+++ src/pages/Usuario/UsuarioPerfil/MeusPedidos/index.jsx
@@ -8,20 +8,22 @@ import { useState, useEffect } from 'react';
 import Avaliacao from './avaliacao/telaAvaliacao';
 import BarraNavegacao from '../../../../components/Usuario/BarraNavegacaoConta';
 import storage from 'local-storage';
+import { toast } from 'react-toastify';
 import { URLRota } from '../../../../constants.js';
 import { Link } from 'react-router-dom';
 
 export default function MeusPedidos () {
 
-    async function avaliacao () {
+    const [idPedido, setIdPedido] = useState(0);
+    const [pedidos, setPedidos] = useState([]);
+
+    async function avaliacao (id) {
         const opcoes = {
-            customUI: () => {
+            customUI: ({onClose}) => {
                 return (
                     <Avaliacao
-                    avaliar={async (numero, idPedido) => {
-                        const url = URLRota + '/pedido/' + idPedido + '/avaliacao/' + numero;
-                        const inserirAvaliacao = await axios.put(url);
-                    }}
+                    idPedido={id}
+                    fechar={onClose}
                     />
 
                 )
@@ -30,7 +32,6 @@ export default function MeusPedidos () {
         confirmAlert(opcoes);
     }
 
-    const [pedidos, setPedidos] = useState([]);
 
     async function chamarPedidos () {
         const idUsuario = storage('usuario-logado').id
@@ -42,6 +43,8 @@ export default function MeusPedidos () {
 
     useEffect(() => {
         chamarPedidos();
+
+        // eslint-disable-next-line 
     }, []);
     
     return (
@@ -74,7 +77,7 @@ export default function MeusPedidos () {
                                 <div className='links'>
                                     <h4 style={ {color: '#0071A1', textDecoration: 'underline', cursor: 'pointer'}}>Exibir detalhes do pedido</h4>
                                     {(item.situacao == 'Entregue') 
-                                    ? <h4 style={ {color: '#0071A1', textDecoration: 'underline', cursor: 'pointer'}} onClick={avaliacao}>Avalie o pedido</h4>
+                                    ? <h4 style={ {color: '#0071A1', textDecoration: 'underline', cursor: 'pointer'}} onClick={() => {avaliacao(item.id);}}>Avalie o pedido</h4>
                                     : <></>
                                     }
                                 </div>
